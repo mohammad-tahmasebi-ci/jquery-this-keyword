d6bdb57e552ce6927b0ac678a0640a93748fe346
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-10 21:21:49 +0000

    after paragraph and button tests using this keyword

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+$('p').click(function() {
+    $('p').slideToggle();
+});
+
+$('p').click(function() {
+    $(this).slideToggle('slow');
+});
+*/
+$('button').mouseenter(function() {
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+$('button').mouseleave(function() {
+    $(this).removeClass('makeBlue').addClass('makeRed');
+});
